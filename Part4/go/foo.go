7ec0f06718c962dd2eb89ddc3c8b122f0a45fb40
vetlemt <55287185+vetlemt@users.noreply.github.com>
2020-01-17 20:40:39 +0100

    Threads working in three lang.

--- Part4/go/foo.go
+++ Part4/go/foo.go
@@ -3,29 +3,39 @@
 package main
 
 import (
-    . "fmt"
-    "runtime"
-    "time"
+	. "fmt"
+	"runtime"
+	"time"
 )
 
 var i = 0
 
 func incrementing() {
-    //TODO: increment i 1000000 times
+	//TODO: increment i 1000000 times
+	for j := 0; j < 1e6; j++ {
+		i++
+	}
 }
 
 func decrementing() {
-    //TODO: decrement i 1000000 times
+	//TODO: decrement i 1000000 times
+	for j := 0; j < 1e6; j++ {
+		i--
+	}
 }
 
 func main() {
-    runtime.GOMAXPROCS(runtime.NumCPU())    // I guess this is a hint to what GOMAXPROCS does...
-	                                    // Try doing the exercise both with and without it!
+	//runtime.GOMAXPROCS(runtime.NumCPU()) // I guess this is a hint to what GOMAXPROCS does...
+	// Try doing the exercise both with and without it!
 
-    // TODO: Spawn both functions as goroutines
-	
-    // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
-    // We'll come back to using channels in Exercise 2. For now: Sleep.
-    time.Sleep(100*time.Millisecond)
-    Println("The magic number is:", i)
+	// TODO: Spawn both functions as goroutines
+	go incrementing()
+	go decrementing()
+	// We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
+	// We'll come back to using channels in Exercise 2. For now: Sleep.
+	time.Sleep(100 * time.Millisecond)
+
+	Println("The magic number is:", i)
+
+	Println("numCPU:", runtime.NumCPU())
 }
--- Part4/python/foo.py
+++ Part4/python/foo.py
@@ -12,20 +12,26 @@ i = 0
 def incrementingFunction():
     global i
     # TODO: increment i 1_000_000 times
+    for _ in range(int(1e6)) : 
+        i += 1
 
 def decrementingFunction():
     global i
     # TODO: decrement i 1_000_000 times
-
-
+    for _ in range(int(1e6)) : 
+        i -= 1
 
 def main():
     # TODO: Something is missing here (needed to print i)
+    global i
 
-    incrementing = Thread(target = incrementingFunction, args = (),)
-    decrementing = Thread(target = decrementingFunction, args = (),)
+    incrementing = Thread(target = incrementingFunction, args = ())
+    decrementing = Thread(target = decrementingFunction, args = ())
     
     # TODO: Start both threads
+
+    incrementing.start()
+    decrementing.start()
     
     incrementing.join()
     decrementing.join()
